197ca43e8319eb4cf786693f5dfe1e392edcf299
Martin Vysny <mavi@vaadin.com>
2017-06-04 14:57:14 +0300

    Added skeletal classes to the project

---
+++ web/src/main/kotlin/com/example/vok/WelcomeView.kt
@@ -0,0 +1,28 @@
+package com.example.vok
+
+import com.github.vok.karibudsl.*
+import com.vaadin.navigator.View
+import com.vaadin.navigator.ViewChangeListener
+import com.vaadin.server.ClassResource
+import com.vaadin.shared.Version
+import com.vaadin.ui.Alignment
+import com.vaadin.ui.VerticalLayout
+import com.vaadin.ui.themes.ValoTheme
+
+@AutoView("")
+class WelcomeView: VerticalLayout(), View {
+    init {
+        verticalLayout {
+            alignment = Alignment.MIDDLE_CENTER
+            isMargin = true; isSpacing = true; defaultComponentAlignment = Alignment.MIDDLE_CENTER
+            label("Yay! You're on Vaadin-on-Kotlin!") {
+                styleName = ValoTheme.LABEL_H1
+            }
+            image(resource = ClassResource("chucknorris.jpg"))
+            label { html("<strong>Vaadin version: </strong> ${Version.getFullVersion()}") }
+            label { html("<strong>Kotlin version: </strong> $kotlinVersion") }
+        }
+    }
+    override fun enter(event: ViewChangeListener.ViewChangeEvent?) {
+    }
+}
