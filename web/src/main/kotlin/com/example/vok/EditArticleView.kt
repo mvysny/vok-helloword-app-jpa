a39c06915011281adfb243a6fdab4a0b336683d3
Martin Vysny <mavi@vaadin.com>
2017-06-05 18:11:01 +0300

    Factored out component ArticleEditor

--- web/src/main/kotlin/com/example/vok/EditArticleView.kt
+++ web/src/main/kotlin/com/example/vok/EditArticleView.kt
@@ -1,48 +1,22 @@
 package com.example.vok
 
-import com.github.vok.framework.db
 import com.github.vok.karibudsl.*
-import com.vaadin.navigator.View
-import com.vaadin.navigator.ViewChangeListener
-import com.vaadin.server.UserError
+import com.vaadin.navigator.*
 import com.vaadin.ui.VerticalLayout
 import com.vaadin.ui.themes.ValoTheme
 
 @AutoView
 class EditArticleView : VerticalLayout(), View {
-    private val binder = beanValidationBinder<Article>()
-    private var article: Article? = null
+    private val editor: ArticleEditor
     init {
         label("Edit Article") {
             styleName = ValoTheme.LABEL_H1
         }
-        textField("Title") {
-            bind(binder).bind(Article::title)
-        }
-        textArea("Text") {
-            bind(binder).bind(Article::text)
-        }
-        button("Save Article", { event ->
-            val article = Article()
-            if (binder.validate().isOk && binder.writeBeanIfValid(article)) {
-                db { em.persist(article) }
-                ArticleView.navigateTo(article.id!!)
-            } else {
-                event.button.componentError = UserError("There are invalid fields")
-            }
-        })
-        button("Back", { navigateToView<ArticlesView>() }) {
-            styleName = ValoTheme.BUTTON_LINK
-        }
+        editor = articleEditor()
     }
     override fun enter(event: ViewChangeListener.ViewChangeEvent) {
         val articleId = event.parameterList[0]!!.toLong()
-        edit(Article.find(articleId)!!)
-    }
-
-    private fun edit(article: Article) {
-        this.article = article
-        binder.readBean(article)
+        editor.article = Article.find(articleId)
     }
 
     companion object {
